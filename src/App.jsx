8621e6c26b53f70a3be6c3f8cf977e276488ebfd
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-16 17:15:40 -0400

    Create state for patients

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+
 import NewPatientForm from "./NewPatientForm";
 
 import "./App.css";
 
 function App() {
+  const [patients, setPatients] = useState([]);
+
   return (
     <>
       <NewPatientForm />
+      <hr />
+      <div>
+        {patients.map((patient) => {
+          return (
+            <div key={patient.id}>
+              <span>{patient.firstName}</span>
+              &nbsp;
+              <span>{patient.lastName}</span>
+              &nbsp;
+              <span>{patient.phoneNumber}</span>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
