78db074a4b7e2da4a5f9a8b1c627c2bc665ed5b7
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-16 16:03:29 -0400

    Create references for form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
+import { useRef } from "react";
+
 import "./App.css";
 
 function App() {
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const phoneNumberRef = useRef();
+
   return (
     <>
       <form action="">
         <div>
           <label htmlFor="">First name</label>
-          <input type="text" />
+          <input ref={firstNameRef} type="text" />
         </div>
         <div>
           <label htmlFor="">Last name</label>
-          <input type="text" />
+          <input ref={lastNameRef} type="text" />
         </div>
         <div>
           <label htmlFor="">Phone number</label>
-          <input type="tel" />
+          <input ref={phoneNumberRef} type="tel" />
         </div>
         <input type="submit" value={"Add"} />
       </form>
