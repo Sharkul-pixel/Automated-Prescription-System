07ae38bb23f30fc0f6e0c487620274ea4436b5eb
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-16 15:59:37 -0400

    Add a simple form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import "./App.css";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <form action="">
+        <div>
+          <label htmlFor="">First name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Last name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Phone number</label>
+          <input type="tel" />
+        </div>
+        <input type="submit" value={"Add"} />
+      </form>
+    </>
+  );
+}
 
 export default App;
