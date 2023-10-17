91d43e38007effa6d5ac3e8ca6d1575700e863f1
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-17 11:17:45 -0400

    Set up BrowserRouter with a root route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import Root from "./routes/Root";
+import "./index.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Root />}></Route>)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
