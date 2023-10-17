b5950cf8be26cc132f33f87d6710bace55a57c89
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-17 19:50:51 -0400

    Link PatientList items to Patient components

--- src/routes/PatientList.jsx
+++ src/routes/PatientList.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 export async function loader() {
   const patients = localStorage.getItem("patients");
@@ -12,16 +12,18 @@ export default function PatientList() {
     <div>
       {patients.map((patient) => {
         return (
-          <div
-            className="my-1 cursor-pointer border border-slate-600 py-2 hover:bg-blue-50"
-            key={patient.id}
-          >
-            <span>{patient.firstName}</span>
-            &nbsp;
-            <span>{patient.lastName}</span>
-            &nbsp;
-            <span>{patient.phoneNumber}</span>
-          </div>
+          <Link to={`/patients/${patient.id}`}>
+            <div
+              className="my-1 cursor-pointer border border-slate-600 py-2 hover:bg-blue-50"
+              key={patient.id}
+            >
+              <span>{patient.firstName}</span>
+              &nbsp;
+              <span>{patient.lastName}</span>
+              &nbsp;
+              <span>{patient.phoneNumber}</span>
+            </div>
+          </Link>
         );
       })}
     </div>
