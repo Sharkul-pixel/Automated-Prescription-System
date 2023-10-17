8204b9130290f5e323d3c78620c948bb1a4b8c4a
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-17 19:39:41 -0400

    Create PatientList.jsx

---
+++ src/routes/PatientList.jsx
@@ -0,0 +1,29 @@
+import { useLoaderData } from "react-router-dom";
+
+export async function loader() {
+  const patients = localStorage.getItem("patients");
+  return { patients: JSON.parse(patients) ?? [] };
+}
+
+export default function PatientList() {
+  const { patients } = useLoaderData();
+
+  return (
+    <div>
+      {patients.map((patient) => {
+        return (
+          <div
+            className="my-1 cursor-pointer border border-slate-600 py-2 hover:bg-blue-50"
+            key={patient.id}
+          >
+            <span>{patient.firstName}</span>
+            &nbsp;
+            <span>{patient.lastName}</span>
+            &nbsp;
+            <span>{patient.phoneNumber}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
