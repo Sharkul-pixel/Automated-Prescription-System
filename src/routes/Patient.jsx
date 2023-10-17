6df38746f4f2d554f9f335aaff4bd5832c04c6f5
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-17 19:49:45 -0400

    Add back button to Patient component

--- src/routes/Patient.jsx
+++ src/routes/Patient.jsx
@@ -1,3 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Patient() {
-  return "patient";
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <button
+        className="border"
+        type="button"
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        back
+      </button>
+      <div>patient component</div>
+    </div>
+  );
 }
