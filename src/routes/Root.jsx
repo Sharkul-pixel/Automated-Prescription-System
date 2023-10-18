38d0b406d1029aeda562e2d010c52763db4c3a2a
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-17 23:09:15 -0400

    Replace NewPatientForm in Root with a Link to it

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,24 +1,14 @@
-import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
-
-import NewPatientForm from "./NewPatientForm";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Root() {
-  const [patients, setPatients] = useState(() => {
-    const patients = localStorage.getItem("patients");
-    return JSON.parse(patients) ?? [];
-  });
-
-  useEffect(() => {
-    localStorage.setItem("patients", JSON.stringify(patients));
-  }, [patients]);
-
   return (
     <>
       <div className="flex min-h-screen">
         <aside className="w-1/4 border-r-2">sidebar</aside>
         <div className="w-3/4">
-          <NewPatientForm setPatients={setPatients} />
+          <button className="m-2 border bg-slate-500 p-1 text-white">
+            <Link to={`/patients/new`}>add new patient</Link>
+          </button>
           <hr />
           <button
             onClick={() => {
