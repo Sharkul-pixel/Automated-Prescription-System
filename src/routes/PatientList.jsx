afd4e293bb802c5548d5ba982652769b30e85dd7
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-19 19:20:30 -0400

    Structure PatientList like a table

--- src/routes/PatientList.jsx
+++ src/routes/PatientList.jsx
@@ -11,18 +11,25 @@ export default function PatientList() {
 
   return (
     <div>
+      <div className="flex">
+        <div className="w-1/12 border border-slate-300 p-2 px-3"></div>
+        <div className="w-1/6 border border-slate-300 p-2 px-3">First Name</div>
+        <div className="w-1/6 border border-slate-300 p-2 px-3">Last Name</div>
+        <div className="w-1/6 border border-slate-300 p-2 px-3">
+          Phone Number
+        </div>
+      </div>
       {patients.map((patient) => {
         return (
           <Link key={patient.id} to={`/patients/${patient.id}`}>
             <div
-              className="my-1 cursor-pointer border border-slate-600 py-2 hover:bg-blue-50"
+              className="flex cursor-pointer border border-slate-300 py-2 hover:bg-blue-50"
               key={patient.id}
             >
-              <span>{patient.firstName}</span>
-              &nbsp;
-              <span>{patient.lastName}</span>
-              &nbsp;
-              <span>{patient.phoneNumber}</span>
+              <div className="w-1/12 px-3"></div>
+              <div className="w-1/6 px-3">{patient.firstName}</div>
+              <div className="w-1/6 px-3">{patient.lastName}</div>
+              <div className="w-1/6 px-3">{patient.phoneNumber}</div>
             </div>
           </Link>
         );
