80caf236b9b8fc31ad00f20b1ad8990915aab6e8
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-19 12:29:49 -0400

    Create GET /patients route

--- api/app.ts
+++ api/app.ts
@@ -1,13 +1,17 @@
 import cors from "cors";
 import express from "express";
 
+import { PrismaClient } from "@prisma/client";
+
 const app = express();
 const port = 3000;
+const prisma = new PrismaClient();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("test");
+app.get("/patients", async (req, res) => {
+  const patients = await prisma.patient.findMany();
+  return res.json(patients);
 });
 
 app.listen(port, () => {
