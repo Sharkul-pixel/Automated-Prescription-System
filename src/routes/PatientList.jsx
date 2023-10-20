14d3e8ca51814d147429275d59d596f89ffe2cdb
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-19 22:07:53 -0400

    Move add patient button to PatientList component

--- src/routes/PatientList.jsx
+++ src/routes/PatientList.jsx
@@ -11,6 +11,25 @@ export default function PatientList() {
 
   return (
     <div>
+      <Link to={`/patients/new`}>
+        <button className="m-2 flex items-center rounded-lg border border-2 border-[#3a92ff] bg-[#4a9bff] px-6 py-1.5 text-white hover:bg-[#2989ff]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="mr-1 h-5 w-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+          <span>Add patient</span>
+        </button>
+      </Link>
       <div className="flex">
         <div className="w-1/12 border border-slate-300 p-2 px-3"></div>
         <div className="w-1/6 border border-slate-300 p-2 px-3">First Name</div>
--- src/routes/Patients.jsx
+++ src/routes/Patients.jsx
@@ -47,26 +47,6 @@ export default function Patients() {
           </div>
         </aside>
         <div className="w-3/4">
-          <Link to={`/patients/new`}>
-            <button className="m-2 flex items-center rounded-lg border border-2 border-[#3a92ff] bg-[#4a9bff] px-6 py-1.5 text-white hover:bg-[#2989ff]">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="currentColor"
-                className="mr-1 h-5 w-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 4.5v15m7.5-7.5h-15"
-                />
-              </svg>
-              <span>Add patient</span>
-            </button>
-          </Link>
-          <hr />
           <Outlet />
         </div>
       </div>
