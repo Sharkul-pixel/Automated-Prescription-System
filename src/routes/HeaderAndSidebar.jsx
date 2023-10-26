49eeece590b9e7088243548ef40293762075bec3
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-26 10:29:51 -0400

    Update sidebar design

--- src/routes/HeaderAndSidebar.jsx
+++ src/routes/HeaderAndSidebar.jsx
@@ -19,13 +19,13 @@ export default function HeaderAndSidebar() {
         <div className="mr-2">&lt;header element 2&gt;</div>
       </header>
       <div className="flex min-h-screen">
-        <aside className="w-1/4 border-r-2 bg-[#f3f4f6]">
+        <aside className="w-1/4 bg-slate-100">
           <span>sidebar</span>
           <NavLink
             to={`/patients`}
             className={({ isActive }) =>
-              `item-center flex border border-slate-300 py-2 ${
-                isActive && "bg-[#d7dae1]"
+              `item-center flex border border-slate-300 py-4 ${
+                isActive && "bg-slate-200"
               }`
             }
           >
@@ -40,7 +40,7 @@ export default function HeaderAndSidebar() {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
               />
             </svg>
             <span>Patients</span>
@@ -48,8 +48,8 @@ export default function HeaderAndSidebar() {
           <NavLink
             to={`/messages`}
             className={({ isActive }) =>
-              `item-center flex border border-slate-300 py-2 ${
-                isActive && "bg-[#d7dae1]"
+              `item-center flex border border-slate-300 py-4 ${
+                isActive && "bg-slate-200"
               }`
             }
           >
