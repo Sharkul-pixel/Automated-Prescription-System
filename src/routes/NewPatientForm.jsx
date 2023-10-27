e0225bcfb0da99d6fe5b7420ee01852dca3ddbce
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-26 22:13:03 -0400

    Update NewPatientForm component design

--- src/routes/NewPatientForm.jsx
+++ src/routes/NewPatientForm.jsx
@@ -23,43 +23,65 @@ export default function NewPatientForm() {
 
   return (
     <>
-      <button
-        className="m-1 bg-slate-500 px-2 text-white"
-        type="button"
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        back
-      </button>
-      <Form className="m-1 border border-slate-400 p-2" method="post">
-        <div>
-          <label>First name</label>
-          <input
-            name="firstName"
-            className="border border-slate-400"
-            type="text"
-          />
+      <div className="mb-[56px]">{/* whitespace */}</div>
+      <div className="border-t border-slate-400"></div>
+      <Form className="ml-1 w-1/2 p-2" method="post">
+        <h1 className="mb-4 text-2xl">Add a New Patient</h1>
+        <div className="mb-4 flex items-center">
+          <div className="w-1/3">
+            <label className="text-lg">First name</label>
+          </div>
+          <div className="w-2/3">
+            <input
+              name="firstName"
+              className="w-full rounded border border-slate-400 p-2 py-1"
+              type="text"
+              autoFocus
+            />
+          </div>
         </div>
-        <div>
-          <label>Last name</label>
-          <input
-            name="lastName"
-            className="border border-slate-400"
-            type="text"
-          />
+        <div className="mb-4 flex items-center">
+          <div className="w-1/3">
+            <label className="text-lg">Last name</label>
+          </div>
+          <div className="w-2/3">
+            <input
+              name="lastName"
+              className="w-full rounded border border-slate-400 p-2 py-1"
+              type="text"
+            />
+          </div>
         </div>
-        <div>
-          <label>Phone number</label>
-          <input
-            name="phoneNumber"
-            className="border border-slate-400"
-            type="tel"
-          />
+        <div className="mb-4 flex items-center">
+          <div className="w-1/3">
+            <label className="text-lg">Phone number</label>
+          </div>
+          <div className="w-2/3">
+            <input
+              name="phoneNumber"
+              className="w-full rounded border border-slate-400 p-2 py-1"
+              type="tel"
+            />
+          </div>
         </div>
-        <button className="rounded bg-blue-600 px-2 text-white" type="submit">
-          Add
-        </button>
+        <div className="mt-8 flex">
+          <div className="grow"></div>
+          <button
+            className="mr-1 rounded-md border-2 border-[#3a92ff] bg-[#4a9bff] px-6 py-1.5 text-white hover:bg-[#2989ff]"
+            type="submit"
+          >
+            Submit
+          </button>
+          <button
+            className="ml-1 rounded-md border-2 border-[#798da4] bg-[#8295aa] px-6 py-1.5 text-white hover:bg-[#70869e]"
+            type="button"
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
       </Form>
     </>
   );
