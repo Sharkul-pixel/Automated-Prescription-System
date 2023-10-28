93f9f7e0475a52b6ee3040d749c949dbbb390058
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-28 16:25:39 -0400

    Update sidebar design

--- src/routes/HeaderAndSidebar.jsx
+++ src/routes/HeaderAndSidebar.jsx
@@ -33,13 +33,13 @@ export default function HeaderAndSidebar() {
         </div>
       </header>
       <div className="flex min-h-screen">
-        <aside className="w-1/4 border-r border-slate-300 bg-slate-100">
-          <span>sidebar</span>
+        <aside className="flex w-1/4 flex-col border-r border-slate-300 bg-slate-100">
+          <div className="ml-3 py-2 text-slate-500">Pharmacy name</div>
           <NavLink
             to={`/patients`}
             className={({ isActive }) =>
-              `item-center flex border-y border-slate-300 py-4 ${
-                isActive && "bg-slate-200"
+              `flex items-center border-y border-slate-300 px-3 py-5 hover:bg-slate-200 ${
+                isActive && "bg-[#dbebff] hover:bg-[#dbebff]"
               }`
             }
           >
@@ -49,7 +49,7 @@ export default function HeaderAndSidebar() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="h-6 w-6"
+              className="h-7 w-7"
             >
               <path
                 strokeLinecap="round"
@@ -57,13 +57,13 @@ export default function HeaderAndSidebar() {
                 d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
               />
             </svg>
-            <span>Patients</span>
+            <span className="mx-1.5 text-lg">Patients</span>
           </NavLink>
           <NavLink
             to={`/messages`}
             className={({ isActive }) =>
-              `item-center flex border-b border-slate-300 py-4 ${
-                isActive && "bg-slate-200"
+              `flex items-center border-y border-slate-300 px-3 py-5 hover:bg-slate-200 ${
+                isActive && "bg-[#dbebff] hover:bg-[#dbebff]"
               }`
             }
           >
@@ -73,7 +73,7 @@ export default function HeaderAndSidebar() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="h-6 w-6"
+              className="h-7 w-7"
             >
               <path
                 strokeLinecap="round"
@@ -81,13 +81,13 @@ export default function HeaderAndSidebar() {
                 d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
               />
             </svg>
-            <span>Messages</span>
+            <span className="mx-1.5 text-lg">Messages</span>
           </NavLink>
           <NavLink
             to={`/settings`}
             className={({ isActive }) =>
-              `item-center flex border-b border-slate-300 py-4 ${
-                isActive && "bg-slate-200"
+              `flex items-center border-y border-slate-300 px-3 py-5 hover:bg-slate-200 ${
+                isActive && "bg-[#dbebff] hover:bg-[#dbebff]"
               }`
             }
           >
@@ -97,7 +97,7 @@ export default function HeaderAndSidebar() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="h-6 w-6"
+              className="h-7 w-7"
             >
               <path
                 strokeLinecap="round"
@@ -110,7 +110,7 @@ export default function HeaderAndSidebar() {
                 d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
               />
             </svg>
-            <span>Settings</span>
+            <span className="mx-1.5 text-lg">Settings</span>
           </NavLink>
         </aside>
         <div className="w-3/4">
