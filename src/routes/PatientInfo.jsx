191c4a3c7452bf68a6d117b7f7609412b4acd2fd
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-29 18:50:13 -0400

    Create loader function and use patient data in JSX

--- src/routes/PatientInfo.jsx
+++ src/routes/PatientInfo.jsx
@@ -1,3 +1,31 @@
+import { Link, useLoaderData } from "react-router-dom";
+
+export async function loader({ params }) {
+  const response = await fetch(
+    `http://localhost:3000/patients/${params.patientId}`,
+  );
+  const patient = await response.json();
+
+  return { patient };
+}
+
 export default function PatientInfo() {
-  return "PatientInfo component";
+  const { patient } = useLoaderData();
+
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl">
+          {patient.firstName} {patient.lastName}
+        </h1>
+        <span>Phone number: {patient.phoneNumber}</span>
+      </div>
+      <Link
+        to={`/patients/${patient.id}/edit`}
+        className="mx-3 mt-3 h-fit w-fit rounded border border-slate-500 px-4 py-1"
+      >
+        Edit
+      </Link>
+    </>
+  );
 }
