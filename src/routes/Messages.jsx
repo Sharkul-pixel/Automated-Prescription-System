4135489c441b217694e6ba7246d59c042110a6b2
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-28 21:32:51 -0400

    Update pagination element

--- src/routes/Messages.jsx
+++ src/routes/Messages.jsx
@@ -28,7 +28,7 @@ export default function Messages() {
           backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%234a9bff' fill-opacity='0.48' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E")`,
         }}
       >
-        <div className="ml-1 flex w-fit rounded-lg border-2 border-slate-300 bg-white px-5 py-1.5">
+        <div className="mx-2 flex w-fit items-center rounded-lg border-2 border-slate-300 bg-white px-8 py-1.5">
           <Link
             className={`rounded border border-slate-400 px-2 py-1 hover:bg-blue-50 ${
               page === 1 &&
@@ -51,11 +51,11 @@ export default function Messages() {
               />
             </svg>
           </Link>
-          <span className="ml-5 w-10 rounded border border-slate-300 py-1 text-center">
+          <span className="ml-5 w-10 rounded border border-slate-300 py-0.5 text-center">
             {page}
           </span>
-          <span className="mx-1 w-6 py-1 text-center">of</span>
-          <span className="mr-5 w-6 py-1 text-center text-slate-600">
+          <span className="mx-1 w-6 text-center">of</span>
+          <span className="mr-5 w-6 text-center text-slate-600">
             {numPages}
           </span>
           <Link
