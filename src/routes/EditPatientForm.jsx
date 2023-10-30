5cb75937a4c5824af93af67e2805754c83f22b99
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-30 15:28:03 -0400

    Design EditPatientForm

--- src/routes/EditPatientForm.jsx
+++ src/routes/EditPatientForm.jsx
@@ -50,35 +50,48 @@ export default function EditPatientForm() {
 
   return (
     <>
-      <Form method="patch" className="w-1/2 border">
-        <div className="flex w-full border">
-          <label className="w-1/3">First name</label>
-          <input
-            className="w-2/3 border"
-            type="text"
-            name="firstName"
-            defaultValue={patient.firstName}
-          />
+      <Form method="patch">
+        <div className="mx-1 mb-4 flex h-[33px] items-center">
+          <span className="grow font-semibold">Update Patient Details</span>
         </div>
-
-        <div className="flex w-full border">
-          <label className="w-1/3">Last name</label>
-          <input
-            className="w-2/3 border"
-            type="text"
-            name="lastName"
-            defaultValue={patient.lastName}
-          />
+        <div className="mx-1 mb-4 flex items-center">
+          <div className="w-1/3">
+            <label>First name</label>
+          </div>
+          <div className="w-2/3">
+            <input
+              name="firstName"
+              className="w-full rounded border border-slate-400 p-2 py-1"
+              type="text"
+              value={patient.firstName}
+            />
+          </div>
         </div>
-
-        <div className="flex w-full border">
-          <label className="w-1/3">Phone number</label>
-          <input
-            className="w-2/3 border"
-            type="tel"
-            name="phoneNumber"
-            defaultValue={patient.phoneNumber}
-          />
+        <div className="mx-1 mb-4 flex items-center">
+          <div className="w-1/3">
+            <label>Last name</label>
+          </div>
+          <div className="w-2/3">
+            <input
+              name="lastName"
+              className="w-full rounded border border-slate-400 p-2 py-1"
+              type="text"
+              value={patient.lastName}
+            />
+          </div>
+        </div>
+        <div className="mx-1 mb-2.5 flex items-center">
+          <div className="w-1/3">
+            <label>Phone number</label>
+          </div>
+          <div className="w-2/3">
+            <input
+              name="phoneNumber"
+              className="w-full rounded border border-slate-400 p-2 py-1"
+              type="tel"
+              value={patient.phoneNumber}
+            />
+          </div>
         </div>
 
         <input
@@ -87,18 +100,26 @@ export default function EditPatientForm() {
           defaultValue={location.state?.returnUrl}
         />
 
-        <button className="m-1 bg-blue-500 px-2 text-white" type="submit">
-          Save
-        </button>
-        <button
-          className="m-1 bg-slate-500 px-2 text-white"
-          type="button"
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          Cancel
-        </button>
+        <div className="mt-3 flex">
+          <div className="grow"></div>
+          <button
+            // className="bg-blue-500 px-2 text-white"
+            className="mr-1 rounded border-2 border-[#3a92ff] bg-[#4a9bff] px-4 text-white hover:bg-[#2989ff]"
+            type="submit"
+          >
+            Save
+          </button>
+          <button
+            // className="m-1 bg-slate-500 px-2 text-white"
+            className="ml-1 rounded border-2 border-[#798da4] bg-[#8295aa] px-4 text-white hover:bg-[#70869e]"
+            type="button"
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
       </Form>
     </>
   );
